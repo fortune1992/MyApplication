2f61d68351c1e9eb5a2590e155a0596f6e882b93
pengguolong <pengguolong@foxmail.com>
2018-05-21 15:43:11 +0800

    Content: test delete file
    Signed-off-by: pengguolong <pengguolong@foxmail.com>
    
    [code test] : add a comment.

--- app/src/main/java/com/example/pengguolong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pengguolong/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // add a comment
         setContentView(R.layout.activity_main);
     }
 
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        //注释1
+        //注释2
+        //注释3
         return true;
     }
 
